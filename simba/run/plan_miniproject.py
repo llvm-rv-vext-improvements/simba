b7c0a2f77dc68895f9a995ab7eb114eb7c2e48b3
Ilnur <Ilnur_ha@mail.ru>
2026-05-13 14:08:06 +0300

    added input data handling while planning; added input data config parsing in miniprojects

--- simba/run/plan_miniproject.py
+++ simba/run/plan_miniproject.py
@@ -2,11 +2,21 @@ from simba.args.argv import RunMiniprojectArgs, RunSourcesArgs, TArgs
 from simba.run.plan_sources import plan_sources
 from simba.run.task import Plan
 from simba.verilator.core import Verilator
+from simba.args.input_data import InputDataConfig, RawInputDataConfig
 
 
 def to_sources_args(args: TArgs[RunMiniprojectArgs]) -> TArgs[RunSourcesArgs]:
     paths = [f.resolve() for f in args.run.path.iterdir()]
-    return TArgs(common=args.common, run=RunSourcesArgs(paths=paths))
+    
+    input_config = args.input_config
+    for path_ in paths:
+        if path_.name == ".input.simba.json":
+            if input_config is None:
+                input_config = InputDataConfig.from_raw(RawInputDataConfig.read_json(path_))
+            paths.remove(path_)
+            break
+
+    return TArgs(common=args.common, run=RunSourcesArgs(paths=paths), input_config=input_config)
 
 
 def plan_miniproject(verilator: Verilator, args: TArgs[RunMiniprojectArgs]) -> Plan:
