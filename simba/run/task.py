5ef894e931565f8c5251e08865ced72486fe9e8c
Ilnur <Ilnur_ha@mail.ru>
2026-05-30 15:45:23 +0300

    fixed usage of mangled __input

--- simba/run/task.py
+++ simba/run/task.py
@@ -37,7 +37,7 @@ def execute_task(task: Task) -> Report:
         return Report(
             name=p.name,
             toolchain=p.toolchain,
-            benchmark_config=p.__input,
+            benchmark_config=p.benchmark_input,
             instrunctions_count=instrs,
             cycles_count=cycles,
             simulation_time=timer.duration,
