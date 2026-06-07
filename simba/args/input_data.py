9e124303593ca26ab2f51e68a6de220a0ce5ef85
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2026-06-07 12:23:32 +0300

    Added passing benchmark config info to report and conversion to csv/html (#13)
    * added benchmark reporting
    
    * fixed usage of mangled __input
    
    * resolved json serealizable problem
    
    * fixed rebase bug
    
    * fixed style checks

--- simba/args/input_data.py
+++ simba/args/input_data.py
@@ -231,6 +231,13 @@ class BenchmarkInput(NamedTuple):
     iterations: IterationsInput
     vars: Tuple[BenchmarkVar, ...]
 
+    def to_csv_str(self) -> str:
+        func_str = f"{self.function.name}({self.function.return_type})"
+        iter_str = f"{self.iterations.warmup}/{self.iterations.main}"
+        var_strs = [f"{v.variable}:{v.var_type}:{v.input_path.name}" for v in self.vars]
+        vars_str = f"[{', '.join(var_strs)}]"
+        return f"{func_str} | iters={iter_str} | vars={vars_str}"
+
     @classmethod
     def from_raw(cls, raw: RawBenchmarkInput) -> "BenchmarkInput":
         return BenchmarkInput(
@@ -238,13 +245,6 @@ class BenchmarkInput(NamedTuple):
             iterations=IterationsInput.from_raw(raw.iterations),
             vars=tuple(BenchmarkVar.from_raw(var_) for var_ in raw.vars),
         )
-
-    def to_csv_str(self) -> str:
-        func_str = f"{self.function.name}({self.function.return_type})"
-        iter_str = f"{self.iterations.warmup}/{self.iterations.main}"
-        var_strs = [f"{v.variable}:{v.var_type}:{v.input_path.name}" for v in self.vars]
-        vars_str = f"[{', '.join(var_strs)}]"
-        return f"{func_str} | iters={iter_str} | vars={vars_str}"
 
 
 type BenchmarkInputs = List[BenchmarkInput]
